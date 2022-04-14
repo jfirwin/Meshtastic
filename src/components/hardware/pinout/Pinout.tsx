6adbd9d9a6b9fc5be1c996966b35b3b216dabbb4
Foster Irwin <foster@jfirwin.com>
2022-04-14 12:47:38 -0600

    work in progress

--- src/components/hardware/pinout/Pinout.tsx
+++ src/components/hardware/pinout/Pinout.tsx
@@ -1,65 +1,39 @@
 import React from 'react';
-import { CirclePath } from '@site/src/utils/pinout'
+import { CirclePath, MeshtasticLogoPath, BoardPath } from '@site/src/utils/pinout'
+import { PinoutLabel } from '@site/src/components/hardware/pinout/PinoutLabel'
 
 export const Pinout = (gpio): JSX.Element => {
 
-gpio = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+gpio = [
+  {side: "left", silkscreen: "LoRa 1", gpio: '1', meshtastic: "PIN_UP_BUTTON"},
+  {side: "right", silkscreen: "GND", gpio: '2', meshtastic: "THIS_MESSAGE"},
+  {side: "left", silkscreen: "3V3", gpio: '3'},
+  {side: "right", silkscreen: "5V", gpio: '4'}
+]
 
 const dimensions = {
   height: "500",
   width: "500",
   boardHeight: 50,
   adjustedBoardHeight: null,
-  boardWidth: 90,
+  boardWidth: 120,
   adjustedBoardWidth: 90,
-  cornerOffset: 9,
-  halfCornerOffset: 9 / 2,
-  cutoutRadius: 5,
-  pinCutoutRadius: 2,
-  pinInterval: 15,
+  cutoutRadius: 6,
+  pinRadius: 3,
+  pinInterval: 30,
+  silkscreenWidth: 30,
   xOffset: 0, // used for centering board on svg
   yOffset: 0, // used for centering board on svg
-  logoWidth: 40,
+  logoWidth: 35,
   logoScale: 1,
-}
-
-const centeredMeshtasticLogo = () => {
-  let logoWidth = 30 * dimensions.logoScale
-  let logoHeight = 15.828 * dimensions.logoScale
-  let x = dimensions.xOffset + (10.049 * dimensions.logoScale) + (dimensions.adjustedBoardWidth / 2) - (logoWidth /2)
-  let y = dimensions.yOffset + (dimensions.adjustedBoardHeight / 2)
-
-  return `M ${x} ${y} l ${1.797 * dimensions.logoScale} ${1.225 * dimensions.logoScale} l ${-9.949 * dimensions.logoScale} ${14.59 * dimensions.logoScale} l ${-1.797 * dimensions.logoScale} ${-1.225 * dimensions.logoScale} l ${9.949 * dimensions.logoScale} ${-14.59 * dimensions.logoScale} l ${-9.949 * dimensions.logoScale} ${0 * dimensions.logoScale} z  m ${9.719 * dimensions.logoScale} ${3.337 * dimensions.logoScale} l ${-8.517 * dimensions.logoScale} ${12.491 * dimensions.logoScale} l ${-1.796 * dimensions.logoScale} ${-1.225 * dimensions.logoScale} l ${9.413 * dimensions.logoScale} ${-13.805 * dimensions.logoScale} c ${0.202236 * dimensions.logoScale} ${-0.29671931 * dimensions.logoScale} ${0.538164 * dimensions.logoScale} ${-0.47433818 * dimensions.logoScale} ${0.897234 * dimensions.logoScale} ${-0.47478038 * dimensions.logoScale} c ${0.359219 * dimensions.logoScale} ${-0.0002948 * dimensions.logoScale} ${0.695295 * dimensions.logoScale} ${0.17673445 * dimensions.logoScale} ${0.89812 * dimensions.logoScale} ${0.47315896 * dimensions.logoScale} l ${9.435 * dimensions.logoScale} ${13.784 * dimensions.logoScale} l ${-1.794 * dimensions.logoScale} ${1.228 * dimensions.logoScale} l ${-8.537 * dimensions.logoScale} ${-12.471 * dimensions.logoScale} z`
 }
 
 dimensions.adjustedBoardHeight = dimensions.boardHeight + (dimensions.pinInterval * (gpio.length))
 dimensions.xOffset = (dimensions.width / 2) - (dimensions.boardWidth / 2)
 dimensions.yOffset = (dimensions.height / 2) - (dimensions.adjustedBoardHeight / 2)
+dimensions.logoWidth = dimensions.boardWidth - (dimensions.silkscreenWidth * 2.5)
 dimensions.logoScale = dimensions.logoWidth / 30
 
-const corners = {
-  topLeft: `C ${dimensions.halfCornerOffset + dimensions.xOffset} ${dimensions.yOffset -0.5} ${dimensions.xOffset -0.5} ${dimensions.halfCornerOffset + dimensions.yOffset} ${dimensions.xOffset} ${dimensions.cornerOffset + dimensions.yOffset}`,
-  bottomLeft: `C ${dimensions.xOffset} ${(dimensions.adjustedBoardHeight - dimensions.halfCornerOffset) + dimensions.yOffset +0.5} ${dimensions.halfCornerOffset + dimensions.xOffset -0.5} ${dimensions.adjustedBoardHeight + dimensions.yOffset} ${dimensions.cornerOffset + dimensions.xOffset} ${dimensions.adjustedBoardHeight + dimensions.yOffset}`,
-  bottomRight: `C ${(dimensions.boardWidth - dimensions.halfCornerOffset) + dimensions.xOffset} ${dimensions.adjustedBoardHeight + dimensions.yOffset +0.5} ${dimensions.boardWidth + dimensions.xOffset +0.5} ${(dimensions.adjustedBoardHeight - dimensions.halfCornerOffset) + dimensions.yOffset} ${dimensions.boardWidth + dimensions.xOffset} ${(dimensions.adjustedBoardHeight - dimensions.cornerOffset) + dimensions.yOffset}`,
-  topRight: `C ${dimensions.boardWidth + dimensions.xOffset} ${dimensions.halfCornerOffset + dimensions.yOffset -0.5} ${(dimensions.boardWidth - dimensions.halfCornerOffset) + dimensions.xOffset +0.5} ${dimensions.yOffset} ${(dimensions.boardWidth - dimensions.cornerOffset) + dimensions.xOffset} ${dimensions.yOffset}`
-}
-const path = [
-  `M ${dimensions.cornerOffset + dimensions.xOffset} ${dimensions.yOffset}`, // Start point (x,y)
-  `${corners.topLeft}`, // top left corner
-  `L ${dimensions.xOffset} ${(dimensions.adjustedBoardHeight - dimensions.cornerOffset) + dimensions.yOffset/*141.73242*/}`, // Left adjustedBoardHeight
-  `${corners.bottomLeft}`, // bottom left corner
-  `L ${(dimensions.boardWidth -  dimensions.cornerOffset) + dimensions.xOffset} ${dimensions.adjustedBoardHeight + dimensions.yOffset}`, // bottom boardWidth
-  `${corners.bottomRight}`, // bottom right corner
-  `L ${dimensions.boardWidth + dimensions.xOffset} ${dimensions.cornerOffset + dimensions.yOffset}`, // right adjustedBoardHeight
-  `${corners.topRight}`, // top right corner
-  `L ${dimensions.cornerOffset + dimensions.xOffset} ${dimensions.yOffset}`, // top boardWidth
-  `z`, // close path
-  centeredMeshtasticLogo(),
-  CirclePath((dimensions.cutoutRadius * 3) + dimensions.xOffset, (dimensions.cutoutRadius * 2) + dimensions.yOffset ,dimensions.cutoutRadius), // top left corner cutout
-  CirclePath(dimensions.boardWidth - (dimensions.cutoutRadius) + dimensions.xOffset, (dimensions.cutoutRadius * 2) + dimensions.yOffset ,dimensions.cutoutRadius), // top right corner cutout
-  CirclePath(dimensions.boardWidth - dimensions.cutoutRadius + dimensions.xOffset, dimensions.adjustedBoardHeight - (dimensions.cutoutRadius * 2) + dimensions.yOffset ,dimensions.cutoutRadius), // bottom right corner cutout
-  CirclePath((dimensions.cutoutRadius * 3) + dimensions.xOffset, dimensions.adjustedBoardHeight - (dimensions.cutoutRadius * 2) + dimensions.yOffset ,dimensions.cutoutRadius), // bottom left corner cutout
-]
 
 const key = [
   {name: "GPIO", color: "Pink"},
@@ -72,19 +46,6 @@ const key = [
   {name: "Sensor", color: "Orange"},
 ]
 
-gpio.map((e, index) => {
-  path.push(CirclePath(
-    (dimensions.pinCutoutRadius * 10) + dimensions.xOffset, // x
-    (dimensions.boardHeight / 2) + (dimensions.pinInterval / 2) + ((index) * (dimensions.pinInterval)) + dimensions.yOffset, // y
-    dimensions.pinCutoutRadius // radius
-  )) // Left side
-  path.push(CirclePath(
-    dimensions.boardWidth - (dimensions.pinCutoutRadius * 9) + dimensions.xOffset, // x
-    (dimensions.boardHeight / 2) + (dimensions.pinInterval / 2) + ((index) * (dimensions.pinInterval)) + dimensions.yOffset, // y
-    dimensions.pinCutoutRadius // radius
-  )) // Right side
-})
-
   return (
     <svg height={dimensions.height} width={dimensions.width}>
     <defs xmlns="http://www.w3.org/2000/svg" id="defs2476">
@@ -94,9 +55,6 @@ gpio.map((e, index) => {
       </linearGradient>
       <linearGradient xlinkHref="#boardGradient" id="boardGradient" x1={(dimensions.boardWidth / 2) + dimensions.xOffset} y1={dimensions.adjustedBoardHeight + dimensions.yOffset} x2={(dimensions.boardWidth / 2) + dimensions.xOffset} y2={dimensions.yOffset - 75} gradientUnits="userSpaceOnUse"/>
     </defs>
-      <rect fill="rgb(103, 234, 148)" height={dimensions.logoWidth} width={dimensions.logoWidth} x={dimensions.xOffset + (dimensions.adjustedBoardWidth / 2) - (dimensions.logoWidth /2)} y={dimensions.yOffset + (dimensions.adjustedBoardHeight /2) - dimensions.logoWidth/3}/>
-
-
       <rect height={dimensions.height} width={dimensions.width} fill="none"/>
       <g>
         {key.map((e, index) => {
@@ -110,36 +68,13 @@ gpio.map((e, index) => {
           )
         })}
       </g>
-      <path d={path.join(" ")} fill="url(#boardGradient)"/>
-      {gpio.map((e, index) => {
-        return (
-          <circle
-            cx={(dimensions.pinCutoutRadius * 10) - dimensions.pinCutoutRadius + dimensions.xOffset}
-            cy={(dimensions.boardHeight / 2) + (dimensions.pinInterval / 2) + ((index) * (dimensions.pinInterval)) + dimensions.yOffset}
-            r={dimensions.pinCutoutRadius}
-            stroke="#c18b30"
-            strokeWidth={dimensions.pinCutoutRadius}
-            fill="none"
-          />
-        )
-      })}
+      <path d={BoardPath(dimensions, gpio).join(" ")} fill="url(#boardGradient)"/>
       {gpio.map((e, index) => {
         let y = (dimensions.boardHeight / 2) + (dimensions.pinInterval / 2) + ((index) * (dimensions.pinInterval)) + dimensions.yOffset
-        let x = dimensions.boardWidth - (dimensions.pinCutoutRadius * 9) - dimensions.pinCutoutRadius + dimensions.xOffset
-        return (
-          <g>
-            <circle
-              cx={x}
-              cy={y}
-              r={dimensions.pinCutoutRadius}
-              stroke="#c18b30"
-              strokeWidth={dimensions.pinCutoutRadius}
-              fill="none"
-            />
-            <text fill="var(--tw-prose-body)" x={x + (dimensions.pinCutoutRadius * 1.5)} y={y + (dimensions.pinInterval / 4)} font-size="10px" textLength={(dimensions.adjustedBoardWidth + dimensions.xOffset) - (x + (dimensions.pinCutoutRadius * 1.5))} lengthAdjust="spacingAndGlyphs">LoRa 1</text>
-          </g>
-        )
+        let x = dimensions.boardWidth - dimensions.silkscreenWidth - dimensions.pinRadius + dimensions.xOffset
+        return PinoutLabel(e, y, dimensions)
       })}
+      <path d={MeshtasticLogoPath(dimensions)} fill="rgb(103, 234, 148)"/>
     </svg>
   );
 };
---
+++ src/components/hardware/pinout/PinoutLabel.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+
+export const PinoutLabel = (gpio, y, dimensions): JSX.Element => {
+  let placement = {
+    fontSize: dimensions.pinInterval - 2,
+    boxHeight: dimensions.pinInterval - 2,
+    labelMargin: 10,
+    pinCircle: {
+      color: "#c18b30",
+      width: dimensions.pinRadius,
+      x: null,
+    },
+    gpioLabel: {
+      width: dimensions.pinInterval,
+      x: null,
+    },
+    silkscreenLabel: {
+      width: dimensions.silkscreenWidth,
+      x: null,
+    },
+    meshtasticLabel: {
+      width: 100,
+      x: null,
+    },
+  }
+  if (gpio.side == "left") {
+    placement.gpioLabel.x = dimensions.xOffset - placement.gpioLabel.width - placement.labelMargin
+    placement.pinCircle.x = dimensions.xOffset + dimensions.silkscreenWidth
+    placement.silkscreenLabel.x = dimensions.xOffset
+  }
+  if (gpio.side == "right") {
+    placement.gpioLabel.x = dimensions.xOffset + dimensions.boardWidth + placement.labelMargin
+    placement.pinCircle.x = dimensions.xOffset + dimensions.boardWidth - dimensions.silkscreenWidth
+    placement.silkscreenLabel.x = dimensions.xOffset
+  }
+
+  if (gpio["meshtastic"])  {
+    console.log("gpio is meshtastic")
+    placement.pinCircle.color = "rgb(103, 234, 148)"
+  }
+
+  console.log(placement.pinCircle.x, dimensions.xOffset)
+  let fontSize = dimensions.pinInterval - 4
+  let boxHeight = dimensions.pinInterval - 4
+  return (
+    <g>
+      <circle
+        r={dimensions.pinRadius}
+        cx={placement.pinCircle.x}
+        cy={y}
+        stroke={placement.pinCircle.color}
+        strokeWidth={dimensions.pinRadius}
+        fill="none"
+      />
+      <path/>
+
+      <text
+        x={placement.silkscreenLabel.x}
+        y={y + (placement.fontSize / 3)}
+        fill="white"
+        fontSize={placement.fontSize}
+        textLength={dimensions.silkscreenWidth}
+        lengthAdjust="spacingAndGlyphs"
+        fillRule="yellow"
+      >
+        LoRa 1
+      </text>
+
+      <rect x={placement.meshtasticLabel.x} y={y - (placement.fontSize / 2)} fill="blue" height={boxHeight} width={placement.meshtasticLabel.width} ry={6}/>
+      <text x={placement.meshtasticLabel.x} y={y + (placement.fontSize / 3)} fill="white" fontSize={placement.fontSize} textLength={placement.meshtasticLabel.width} lengthAdjust="spacingAndGlyphs">Meshtastic Label</text>
+
+      <rect x={placement.gpioLabel.x} y={y - (placement.fontSize / 2)} fill="pink" height={boxHeight} width={placement.gpioLabel.width} ry={6}/>
+      <text x={placement.gpioLabel.x} y={y + (placement.fontSize / 3)} fill="white" fontSize={placement.fontSize} textLength={placement.gpioLabel.width} lengthAdjust="spacingAndGlyphs">LoRa 1</text>
+
+    </g>
+  )
+}
